f6ac416b3773bb904b5b4b03798a4d99bc8b2e27
LEONID NIFANTIJEV <leonif1@gmail.com>
2023-11-07 20:00:56 +0200

    remove storyboard

--- search-app/Main/MainViewController.swift
+++ search-app/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  search-app
 //
 //  Created by LEONID NIFANTIJEV on 07.11.2023.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
-
-
 }
 
